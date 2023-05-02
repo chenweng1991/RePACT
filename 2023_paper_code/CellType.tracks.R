2130d48e272ab5c620369cf46d1fa135d00c8138
Shanshan Zhang <39412563+shanshan950@users.noreply.github.com>
2023-05-02 11:14:28 -0400

    Create CellType.tracks.R

---
+++ 2023_paper_code/CellType.tracks.R
@@ -0,0 +1,20 @@
+"
+~/Chip-seq/ATAC/scATAC/IsletscATAC/MakeTrack.Step1.pl SVM.KNN.filtered.dic.Final.tab  SVM.KNN.filteredFinal
+~/Chip-seq/ATAC/scATAC/IsletscATAC/MakeTrack.Step2.sh Acinar.SVM.KNN.filteredFinal.bed Alpha.SVM.KNN.filteredFinal.bed Beta.SVM.KNN.filteredFinal.bed Delta.SVM.KNN.filteredFinal.bed Duct.SVM.KNN.filteredFinal.bed PP.SVM.KNN.filteredFinal.bed PSC.SVM.KNN.filteredFinal.bed
+"
+"
+track type=bigWig name=Acinar.SVM.KNN.filteredFinal.sorted.bw description=Acinar.SVM.KNN.filteredFinal.sorted.bw visibility=full autoScale=on maxHeightPixels=100:20:15 bigDataUrl=http://intron.cwru.edu/~fxj45/CW/scATAC2019/tmp.Acinar.SVM.KNN.filteredFinal.sorted.bw color=255,0,0
+
+track type=bigWig name=Alpha.SVM.KNN.filteredFinal.sorted.bw description=Alpha.SVM.KNN.filteredFinal.sorted.bw visibility=full autoScale=on maxHeightPixels=100:20:15 bigDataUrl=http://intron.cwru.edu/~fxj45/CW/scATAC2019/tmp.Alpha.SVM.KNN.filteredFinal.sorted.bw color=0,102,204
+
+track type=bigWig name=Beta.SVM.KNN.filteredFinal.sorted.bw description=Beta.SVM.KNN.filteredFinal.sorted.bw visibility=full autoScale=on maxHeightPixels=100:20:15 bigDataUrl=http://intron.cwru.edu/~fxj45/CW/scATAC2019/tmp.Beta.SVM.KNN.filteredFinal.sorted.bw color=51,153,102
+
+track type=bigWig name=Delta.SVM.KNN.filteredFinal.sorted.bw description=Delta.SVM.KNN.filteredFinal.sorted.bw visibility=full autoScale=on maxHeightPixels=100:20:15 bigDataUrl=http://intron.cwru.edu/~fxj45/CW/scATAC2019/tmp.Delta.SVM.KNN.filteredFinal.sorted.bw color=153,51,255
+
+track type=bigWig name=Duct.SVM.KNN.filteredFinal.sorted.bw description=Duct.SVM.KNN.filteredFinal.sorted.bw visibility=full autoScale=on maxHeightPixels=100:20:15 bigDataUrl=http://intron.cwru.edu/~fxj45/CW/scATAC2019/tmp.Duct.SVM.KNN.filteredFinal.sorted.bw color=255,102,0
+
+track type=bigWig name=PP.SVM.KNN.filteredFinal.sorted.bw description=PP.SVM.KNN.filteredFinal.sorted.bw visibility=full autoScale=on maxHeightPixels=100:20:15 bigDataUrl=http://intron.cwru.edu/~fxj45/CW/scATAC2019/tmp.PP.SVM.KNN.filteredFinal.sorted.bw color=204,102,0
+
+track type=bigWig name=PSC.SVM.KNN.filteredFinal.sorted.bw description=PSC.SVM.KNN.filteredFinal.sorted.bw visibility=full autoScale=on maxHeightPixels=100:20:15 bigDataUrl=http://intron.cwru.edu/~fxj45/CW/scATAC2019/tmp.PSC.SVM.KNN.filteredFinal.sorted.bw color=255,153,204
+
+"
